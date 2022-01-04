68f75a56175c1424d349cf587578590fd5c9213a
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2022-01-04 19:15:47 +0000

    ...

--- src/components/MyProfile/MyProfileTable.js
+++ src/components/MyProfile/MyProfileTable.js
@@ -1,35 +1,87 @@
 import React, { Fragment } from "react";
+import MyProfileForm from "./MyProfileForm";
+import ".././../style.css";
+import "./MyProfileTable.css";
 
 const MyProfileTable = (props) => {
-  const userGameMap = props.currentUserFullProfile[0].gameMap;
-  const userGameStyle = props.currentUserFullProfile[0].gameStyle;
-  const userDays = props.currentUserFullProfile[0].days;
-  const userGamebuddyUsername =
-    props.currentUserFullProfile[0].gamebuddyUsername;
-  const userKD = props.currentUserFullProfile[0].kd;
+  const userProfile = {
+    gamebuddyUsername: props.currentUserFullProfile[0].gamebuddyUsername,
+    warzoneUsername: props.currentUserFullProfile[0].warzoneUsername,
+    platform: props.currentUserFullProfile[0].platform,
+    gameMap: props.currentUserFullProfile[0].gameMap,
+    mode: props.currentUserFullProfile[0].mode,
+    kd: props.currentUserFullProfile[0].kd,
+    gameStyle: props.currentUserFullProfile[0].gameStyle,
+    mic: props.currentUserFullProfile[0].mic,
+    lang: props.currentUserFullProfile[0].lang,
+    days: props.currentUserFullProfile[0].days,
+    time: props.currentUserFullProfile[0].time,
+    timeZone: props.currentUserFullProfile[0].timeZone,
+  };
   return (
     <Fragment>
-      <table className="mt-5 ms-5 text-danger bg-light">
-        <thead className="text-dark ">USER PROFILE</thead>
-        <tbody>
-          <tr>
-            <td>{userGamebuddyUsername}</td>
-          </tr>
-          <tr>
-            <td>{userKD}</td>
-          </tr>
-          <tr>
-            <td>{userDays}</td>
-          </tr>
-
-          <tr>
-            <td>{userGameStyle}</td>
-          </tr>
-          <tr>
-            <td>{userGameMap}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="container">
+        <table class="table text-light transparent-dark mt-7 w-50 ml-10">
+          <tbody>
+            <tr>
+              <th scope="col">Gaming Buddy Username</th>
+              <td>{userProfile.gamebuddyUsername}</td>
+            </tr>
+            <tr>
+              <th scope="col">Warzone Username</th>
+              <td>{userProfile.warzoneUsername}</td>
+            </tr>
+            <tr>
+              <th scope="col">Platform</th>
+              <td>{userProfile.platform}</td>
+            </tr>
+            <tr>
+              <th scope="col">Favourite map</th>
+              <td>{userProfile.gameMap}</td>
+            </tr>
+            <tr>
+              <th scope="col">Favourite mode</th>
+              <td>{userProfile.mode}</td>
+            </tr>
+            <tr>
+              <th scope="col">KD</th>
+              <td>{userProfile.kd}</td>
+            </tr>
+            <tr>
+              <th scope="col">Game Style</th>
+              <td>{userProfile.gameStyle}</td>
+            </tr>
+            <tr>
+              <th scope="col">Microphone</th>
+              <td>{userProfile.mic}</td>
+            </tr>
+            <tr>
+              <th scope="col">Language</th>
+              <td>{userProfile.lang}</td>
+            </tr>
+            <tr>
+              <th scope="col">Availability - Days</th>
+              <td>{userProfile.days}</td>
+            </tr>
+            <tr>
+              <th scope="col">Availability - Time</th>
+              <td>{userProfile.time}</td>
+            </tr>
+            <tr>
+              <th scope="col">Time zone</th>
+              <td>{userProfile.timeZone}</td>
+            </tr>
+          </tbody>
+        </table>
+        <button
+          className="btn btn-outline-light btn-lg background ml-25"
+          data-bs-toggle="modal"
+          data-bs-target="#profileForm"
+        >
+          CREATE PROFILE
+        </button>
+      </div>
+      <MyProfileForm />
     </Fragment>
   );
 };
