0eb32a981abd0540bc70a96449121472176b3466
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2022-01-11 20:48:58 +0000

    ...

--- src/localStorage/storedSignupValues.js
+++ src/localStorage/storedSignupValues.js
@@ -1,1 +1,38 @@
-const storedSignupValues= JSON.parse()
+const storedSignupValues = JSON.parse(
+  {
+    emailAddress: "ale1a1@libero.it",
+    gamebuddyUsername: "a",
+    warzoneUsername: "a",
+    password: "a",
+  },
+  {
+    emailAddress: "ale1a184@gmail.com",
+    gamebuddyUsername: "b",
+    warzoneUsername: "b",
+    password: "b",
+  },
+  {
+    emailAddress: "amwellvets@gmail.com",
+    gamebuddyUsername: "c",
+    warzoneUsername: "c",
+    password: "c",
+  },
+  {
+    emailAddress: "ale1a1@libero.itAAA",
+    gamebuddyUsername: "AAA",
+    warzoneUsername: "AAA",
+    password: "AAA",
+  },
+  {
+    emailAddress: "ale1a184@gmail.comBBB",
+    gamebuddyUsername: "BBB",
+    warzoneUsername: "BBB",
+    password: "BBB",
+  },
+  {
+    emailAddress: "amwellvets@gmail.comCCC",
+    gamebuddyUsername: "CCC",
+    warzoneUsername: "CCC",
+    password: "CCC",
+  }
+);
